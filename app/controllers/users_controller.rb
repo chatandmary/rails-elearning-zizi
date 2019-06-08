c2d748b8c3af0aa1c87a5e6c095bb8179efaa253
zizi <chatandmary@gmail.com>
2019-06-08 15:54:28 +0800

    fixed flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
       if @user.save
+        flash[:warning] = "Created your account!!"
         redirect_to root_url
       else
         render "new"
