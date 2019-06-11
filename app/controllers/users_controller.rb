a4d24f017a4441f5761f8d46db4a501d3d6522f4
zizi <chatandmary@gmail.com>
2019-06-11 12:48:54 +0800

    Installed mini_magick

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
   end
@@ -23,6 +26,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])    
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:primary] = "Updated your profile"
+        redirect_to @user
+      else
+        render "edit"
+      end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:primary] = "User deleted!!"
+    redirect_to users_url
+  end
+
   def following
     @title = "Following"
     @user  = User.find(params[:id])
@@ -41,6 +64,22 @@ class UsersController < ApplicationController
     # strong parameters
     def user_params
         params.require(:user).permit(:name,:email, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 
 end
