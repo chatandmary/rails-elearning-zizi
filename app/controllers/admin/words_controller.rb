fe0bf9a8ee3b4737d904c94166f800f9745c8d0f
zizi <chatandmary@gmail.com>
2019-06-15 15:17:21 +0800

    Updated add_word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -8,10 +8,11 @@ class Admin::WordsController < Admin::ApplicationController
   end
 
   def create
-    @word = Word.new(word_params)
+    @category = Category.find(params[:category_id])
+    @word = @category.word.build(word_params)
     if @word.save
       flash[:success] = "Created new word!!"
-      redirect_to new_admin_category_word_url
+      redirect_to admin_categories_url
     else
       render "new"
     end
@@ -20,7 +21,7 @@ class Admin::WordsController < Admin::ApplicationController
   private 
       # strong parameters
       def word_params
-          params.require(:word).permit(:content, choice_attributes: [:content])
+          params.require(:word).permit(:content, choice_attributes: [:id,:content,:correct])
       end
       
 
