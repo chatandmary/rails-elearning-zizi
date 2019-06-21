99a58788b42a2782dfaeb042f0d2e636c18be05d
zizi <chatandmary@gmail.com>
2019-06-21 12:02:23 +0800

    Add validates for check_box

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,6 +6,16 @@ class Word < ApplicationRecord
   has_many :answers
   has_many :lessons, through: :answers
   has_many :choices, dependent: :destroy
-  accepts_nested_attributes_for :choices  
+  accepts_nested_attributes_for :choices
+
+  validate :check_box
+
+  private
+
+  def check_box
+    unless choices.collect{ |choice| choice.correct }.count(true) == 1
+      errors.add(:choices, "should have only one choice")
+    end
+  end
 
 end
